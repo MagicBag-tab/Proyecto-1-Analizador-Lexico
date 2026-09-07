c2f96cb83255810a7f4e08f6e5a06f7ef0e725ad
MagicBag-tab <est24347@uvg.edu.gt>
2026-09-06 18:37:12 -0600

    feat: añadiendo docker para levantar el proyecto del analizador léxico

--- frt/vite.config.js
+++ frt/vite.config.js
@@ -4,8 +4,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': process.env.BACKEND_URL || 'http://localhost:5000',
     },
   },
 })
